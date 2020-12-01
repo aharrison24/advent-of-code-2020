900836fc5a5da2f6ce4defda3e4e090ab00539f9
Alastair Harrison <aharrison24@gmail.com>
2020-12-01 22:38:25 +0000

    Add solution for day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,32 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+
+    let file = File::open(&args.path).expect("Cannot open file");
+
+    let nums: HashSet<_> = BufReader::new(file)
+        .lines()
+        .filter_map(Result::ok)
+        .filter_map(|x| x.parse::<i32>().ok())
+        .collect();
+
+    for num in &nums {
+        if nums.contains(&(2020 - num)) {
+            println!("Result: {}", num * (2020 - num));
+            return;
+        }
+    }
+
+    println!("No result found");
 }
