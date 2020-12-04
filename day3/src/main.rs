10fe8927a08448d555219c5d3442f2e237eef4de
Alastair Harrison <aharrison24@gmail.com>
2020-12-04 08:36:04 +0000

    Add day3, part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,8 +6,21 @@ use structopt::StructOpt;
 
 #[derive(PartialEq, Debug)]
 struct State {
+    line_index: i32,
     count: i32,
-    xpos: i32,
+    sample_x: i32,
+    sample_y: i32,
+}
+
+impl Default for State {
+    fn default() -> State {
+        State {
+            line_index: 0,
+            count: 0,
+            sample_x: 0,
+            sample_y: 0,
+        }
+    }
 }
 
 #[derive(StructOpt)]
@@ -16,18 +29,38 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn reducer(state: State, line: &str) -> State {
-    let next_xpos = (state.xpos + 3).rem(line.len() as i32);
-    match line.chars().nth(state.xpos as usize).unwrap() {
-        '#' => State {
-            count: state.count + 1,
-            xpos: next_xpos,
-        },
-        _ => State {
-            xpos: next_xpos,
-            ..state
-        },
+fn reducer(acc: State, line: &str, x_step: i32, y_step: i32) -> State {
+    // Skip lines until we hit the next y sample point
+    if acc.line_index < acc.sample_y {
+        return State {
+            line_index: acc.line_index + 1,
+            ..acc
+        };
     }
+
+    // Compute next sample location
+    let result = State {
+        sample_x: (acc.sample_x + x_step).rem(line.len() as i32),
+        sample_y: acc.sample_y + y_step,
+        line_index: acc.line_index + 1,
+        ..acc
+    };
+
+    // Increment count if there is a tree at the current location
+    match line.chars().nth(acc.sample_x as usize).unwrap() {
+        '#' => State {
+            count: acc.count + 1,
+            ..result
+        },
+        _ => result,
+    }
+}
+
+fn count_trees_hit(x_step: i32, y_step: i32, lines: &Vec<String>) -> i32 {
+    lines
+        .iter()
+        .fold(Default::default(), |acc, x| reducer(acc, x, x_step, y_step))
+        .count
 }
 
 fn main() {
@@ -40,8 +73,19 @@ fn main() {
         .filter_map(Result::ok)
         .collect_vec();
 
-    let result = lines.iter().fold(State{count: 0, xpos: 0}, |acc, x| reducer(acc, x));
-    println!("Number of trees hit: {}", result.count);
+    println!(
+        "Part1: Number of trees hit for slope (3,1) = {}",
+        count_trees_hit(3, 1, &lines)
+    );
+
+    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let tree_counts = slopes
+        .iter()
+        .map(|&(x_step, y_step)| count_trees_hit(x_step, y_step, &lines));
+    println!(
+        "Part2: Product of trees hit for multiple slopes = {}",
+        tree_counts.product::<i32>()
+    );
 }
 
 #[cfg(test)]
@@ -51,25 +95,47 @@ mod tests {
     #[test]
     fn dots_do_not_increase_the_count() {
         assert_eq!(
-            reducer(State { count: 0, xpos: 0 }, "...."),
-            State { count: 0, xpos: 3 }
+            reducer(Default::default(), "....", 3, 1),
+            State {
+                line_index: 1,
+                count: 0,
+                sample_x: 3,
+                sample_y: 1
+            }
         );
     }
 
     #[test]
     fn hash_does_increase_the_count() {
         assert_eq!(
-            reducer(State { count: 0, xpos: 0 }, "#..."),
-            State { count: 1, xpos: 3 }
+            reducer(Default::default(), "#...", 3, 1),
+            State {
+                line_index: 1,
+                count: 1,
+                sample_x: 3,
+                sample_y: 1
+            }
         );
     }
 
     #[test]
     fn xpos_wraps_at_line_end() {
         assert_eq!(
-            reducer(State { count: 0, xpos: 1 }, "#..."),
-            State { count: 0, xpos: 0 }
+            reducer(
+                State {
+                    sample_x: 1,
+                    ..Default::default()
+                },
+                "#...",
+                3,
+                1
+            ),
+            State {
+                line_index: 1,
+                count: 0,
+                sample_x: 0,
+                sample_y: 1
+            }
         );
     }
-
 }
